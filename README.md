c702ac7f3611deb14e8e1606a30755329d716166
Anthony Whalley <anton@venshare.com>
2021-02-22 00:24:59 +0000

    initial draft

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # project-stats
 A script to pull stats from github projects
+
+## usage
+
+```bash
+# Clone this repo 
+$ git clone https://github.com/no9/project-stats
+
+# Change into the repo
+$ cd project-stats
+
+# Make an output dir and change into it
+$ mkdir output && cd output
+
+# Get contribution stats 
+# $ npm run <project array>
+$ node ../read.js '["no9/project-stats"]'
+# process downloaded file
+$ node ../read.js
+project-stats,no9,1
+```
--- read.js
+++ read.js
@@ -1,7 +1,44 @@
+// https://api.github.com/repos/No9/harmon/contributors
+
 const path = require('path');
 const fs = require('fs');
+const http = require('https');
 
-fs.readdir(__dirname, function (err, files) {
+var listarg = process.argv[2];
+if (listarg) {
+    var list = JSON.parse(listarg);
+    if (Array.isArray(list)) {
+        list.forEach(function (listitem) {
+            var options = {
+                host: 'api.github.com',
+                path: '/repos/' + listitem + '/contributors',
+                headers: { 'User-Agent': 'Node.js/v14.15.3' }
+            };
+            callback = function(response) {
+                var str = '';
+                //another chunk of data has been received, so append it to `str`
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+            
+                //the whole response has been received, so we just print it out here
+                response.on('end', function () {
+                    var filename = listitem.split('/')[1];
+                    fs.writeFileSync(filename + '.json', str);
+                    console.log(listitem + ' Done!');
+                });
+            }
+            http.request(options, callback).end();
+            
+            //https://api.github.com/repos/No9/harmon/contributors
+        });
+        return console.log("Complete!");
+    } else {
+        return console.log('Unable to scan directory: ' + err);
+    }
+}
+
+fs.readdir(process.cwd(), function (err, files) {
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
@@ -15,7 +52,7 @@ fs.readdir(__dirname, function (err, files) {
             var project = file.replace('.json', '');
             var contribs = JSON.parse(data);
             for (var i=0; i < contribs.length; i++) {
-                console.log(project + "," + contribs[i].author.login + "," + contribs[i].total);
+                console.log(project + "," + contribs[i].login + "," + contribs[i].contributions);
             }
              
         }
